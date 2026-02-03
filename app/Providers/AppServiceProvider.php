104f5f7ec87d775324a7623068a148a9ed2e8754
tonkla2545 <109588556+tonkla2545@users.noreply.github.com>
2026-02-03 20:15:10 +0700

    import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;  // ← ต้อง import ตรงนี้
 
 class AppServiceProvider extends ServiceProvider
 {
